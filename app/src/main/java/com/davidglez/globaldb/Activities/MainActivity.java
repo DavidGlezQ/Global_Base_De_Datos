b38f46df89db06ee6fd01cceb8ad5e529a1c0b5c
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-08-21 16:50:47 -0500

    Me lleva la chingada tengo que iniciar de cero

---
+++ app/src/main/java/com/davidglez/globaldb/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.davidglez.globaldb.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.davidglez.globaldb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
